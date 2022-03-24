c8af4ed46b91ac3fd67548edb3ccd0a77e70506f
salahdin-ahmed <salah2112004@gmail.com>
2022-03-24 22:24:26 +0200

    Change multiple options behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from random import choices
+from random import choices, randint
 from faker import Faker
 from urllib.parse import quote
 
@@ -16,17 +16,30 @@ option.add_argument("-incognito")
 browser = webdriver.Chrome(options=option)
 
 for _ in range(10):
+
     # random choices with different weights
     a = choices(formOptions, [1, 3, 5, 1])[0]
     b = choices(formOptions, [5, 3, 2, 1])[0]
-    c = choices(formOptions, [2, 1, 1, 10])[0]
-    d = choices(formOptions)[0]
     scale = choices(range(1, 6), [1, 2, 3, 10, 5])[0]
+
     # random open-ended answers url percent encoded
     longAnswer = quote(Faker().text(), safe='')
     ShortAnswer = quote(Faker().sentence(), safe='')
+
     # the enteries with answer inside
-    entries = f'entry.98489750={a}&entry.1591486926={longAnswer}&entry.231373470={ShortAnswer}&entry.1781145092={b}&entry.1781145092={c}&entry.1761077668={d}&entry.1907356112={scale}'
+    entries = f'entry.98489750={a}&entry.1591486926={longAnswer}&entry.231373470={ShortAnswer}&entry.1761077668={b}&entry.1907356112={scale}'
+
+    # add a random number of mutliple options
+    # TODO: improve implementation: mutliple options having the same option is
+    # still a posssibility this way, and this does not play well in producing
+    # statsitcally suffecient results. It's only advanatage, however, is the
+    # more organic look of the data.
+    # If this does not appeal to you then modifty the code to reproduce the old
+    # behaviour of a fixed number of options with a statsitcally suffiecent
+    # results and a less bit organic data
+    for _ in range(randint(1, len(formOptions) + 1)):
+        entries += f'&entry.1781145092={choices(formOptions, [7, 3, 4, 2])[0]}'
+
     # open the browser and reload the tab N number of times (submitting N
     # responses)
     browser.get(f"{link}?{entries}")
